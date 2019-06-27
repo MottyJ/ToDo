84cd63a50a4760a62763b468f761c64ee570033e
Motty Jaffe <motty@Mottys-MacBook-Pro.local>
2019-06-27 17:18:59 +0300

    added the ability to add to dos, made a few code design changes, working on the ability to move the dos to ne with a double click

--- logic.js
+++ logic.js
@@ -29,37 +29,52 @@ class Main extends React.Component {
     constructor(props) {
         super(props)
         this.state = {
-            item: "",
             textInput: "",
-            counter: 0
+            listOfItems: [],
+            listOfDone: []
         }
-        this.x = [];
         this.addItem = this.addItem.bind(this)
+        this.checkForEnter = this.checkForEnter.bind(this)
     }
     addItem() {
+        this.state.listOfItems.push(this.state.textInput)
         this.setState({
-            item: this.state.textInput.value,
-            counter: this.state.counter + 1
+            textInput : "",
+            listOfItems : this.state.listOfItems
+        })
+    }
+    checkForEnter(e) {
+        if (e.keyCode === 13) {
+            this.addItem()
+        }
+    }
+    done(e) {
+        this.state.listOfDone.push(e)
+        this.setState({
+            listItems: this.state.listOfItems.splice(e.key)
         })
     }
     render() {
-        for (let i = 0; i < this.state.counter; i++) {
-            this.x.push(<div key={i}>{this.state.item}</div>)
-        }
+        var listItems = this.state.listOfItems.map((item, index) => <li className="to-do-ex" key={index} onDoubleClick={this.done}>{item}</li>)
+        var doneItems = this.state.listOfDone.map((item, index) => <li className="done-ex" key={index}>{item}</li>)
         return (
             <div className="main">
                 <div className="to-do">
                     <h1 className="do-title">do</h1>
-                    <h3 className="to-do-ex">take out the trash</h3>
-                    {this.x}
+                    <ul className="to-do-list">
+                        <li className="to-do-ex">take out the trash</li>
+                        {listItems}
+                    </ul>
                 </div>
                 <div className="input-container">
-                    <input ref={(input) => { this.state.textInput = input; }} name="do" className="my-input" type="text" placeholder="take out the trash..."></input>
-                    <button name="do" onClick={this.addItem} className="add">do</button>
+                    <input value={this.state.textInput} onChange={(e) => this.setState({textInput : e.target.value})} name="do" className="my-input" onKeyUp={this.checkForEnter} type="text" placeholder="take out the trash..."></input>
                 </div>
                 <div className="done">
-                    <h1 className="done-title">done</h1>
-                    <h3 className="done-ex">take out the dog</h3>
+                    <h1 className="done-title">ne</h1>
+                    <ul className="done-list">
+                        <li className="done-ex">take out the dog</li>
+                        {doneItems}
+                    </ul>
                 </div>
             </div>
         );
