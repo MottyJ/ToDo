5eb0825c2fed75d6dcbd2e3bfc3b38f501e61c49
Motty Jaffe <motty@Mottys-MacBook-Pro.local>
2019-06-27 14:13:52 +0300

    added some more styling and the ability to add item to the to do list via state

--- logic.js
+++ logic.js
@@ -19,7 +19,7 @@ class SideMenu extends React.Component {
     render() {
         return (
             <div className="side-menu">
-                
+
             </div>
         );
     }
@@ -28,12 +28,39 @@ class SideMenu extends React.Component {
 class Main extends React.Component {
     constructor(props) {
         super(props)
+        this.state = {
+            item: "",
+            textInput: "",
+            counter: 0
+        }
+        this.x = [];
+        this.addItem = this.addItem.bind(this)
+    }
+    addItem() {
+        this.setState({
+            item: this.state.textInput.value,
+            counter: this.state.counter + 1
+        })
     }
     render() {
+        for (let i = 0; i < this.state.counter; i++) {
+            this.x.push(<div key={i}>{this.state.item}</div>)
+        }
         return (
             <div className="main">
-                <input className="my-input" type="text" placeholder="take out the trash..."></input>
-                <button className="add">do</button>
+                <div className="to-do">
+                    <h1 className="do-title">do</h1>
+                    <h3 className="to-do-ex">take out the trash</h3>
+                    {this.x}
+                </div>
+                <div className="input-container">
+                    <input ref={(input) => { this.state.textInput = input; }} name="do" className="my-input" type="text" placeholder="take out the trash..."></input>
+                    <button name="do" onClick={this.addItem} className="add">do</button>
+                </div>
+                <div className="done">
+                    <h1 className="done-title">done</h1>
+                    <h3 className="done-ex">take out the dog</h3>
+                </div>
             </div>
         );
     }
