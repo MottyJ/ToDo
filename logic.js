6a7402abcbd75193198011a55bacc6c87666c26b
Motty Jaffe <motty@Mottys-MacBook-Pro.local>
2019-06-29 22:14:05 +0300

    added a modal to explain the working of it

--- logic.js
+++ logic.js
@@ -5,8 +5,43 @@ class App extends React.Component {
     render() {
         return (
             <div className="container">
+                <Modal />
                 <SideMenu />
                 <Main />
+            </div>
+        );
+    }
+}
+
+class Modal extends React.Component {
+    constructor(props) {
+        super(props)
+        this.state = {
+            isRead: false
+        }
+        this.confirmRead = this.confirmRead.bind(this)
+    }
+    confirmRead(){
+        this.setState({
+            isRead: true
+        })
+    }
+    render() {
+        var readStatus = this.state.isRead ? "hide-me" : ""
+        return (
+            <div id="modal-wrapper" className={readStatus}>
+                <div className="modal">
+                    <h1>welcome to</h1>
+                    <span>
+                        <span className="red logo">do</span>
+                        <span className="logo"><span className="red">-</span><span className="green">-</span></span>
+                        <span className="green logo">ne</span>
+                    </span>
+                    <h3>Simply type your tasks and press enter to add a do (that's what we like to call to-dos)</h3>
+                    <h3>Double click the tasks you've finished to place them in the ne area(that's what we like to call done tasks)</h3>
+                    <h3>If you'd like to delete a task completely, simply right click on the do/ne and it will be destroyed!</h3>
+                    <button className="modal-button" onClick={this.confirmRead}>do</button>
+                </div>
             </div>
         );
     }
